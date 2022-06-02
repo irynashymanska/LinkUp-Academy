ab1ec1a4536413a6170747a6958b693819dab654
irynashymanska <shymanska04@gmail.com>
2022-06-02 13:02:40 +0300

    fix: task 16

--- JS/task-16.js
+++ JS/task-16.js
@@ -1,15 +1,9 @@
 function tempConversion(c) {
-  let f;
-  let k;
-
   if (typeof c === "number") {
-    f = (c * 9) / 5 + 32;
-    k = c + 273.15;
-    return k>=0 ?  [Number(f.toFixed(2)), Number(k.toFixed(2))] : "'Invalid'"
-   
-  } 
-  else {
-    return "'Invalid'";
+    let f = (c * 9) / 5 + 32;
+    let k = c + 273.15;
+    return k >= 0 ? [Number(f.toFixed(2)), Number(k.toFixed(2))] : "Invalid";
+  } else {
+    return "Invalid";
   }
 }
-
